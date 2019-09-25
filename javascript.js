fa2d22629fe8479eedac70bc7706227878fd4e9a
Daniel <dhuerta1408@gmail.com>
2019-09-25 10:28:52 -0600

    added url functionality to reviews

--- javascript.js
+++ javascript.js
@@ -281,6 +281,7 @@ $("#submit-review").on("click", function (event) {
   var yourName = $("#your-name").val().trim();
   var recipeName = $("#recipe-name").val().trim();
   var comment = $("#review-comment").val().trim();
+  var recipeURL = $("#recipe-url").val().trim();
 
   database.ref().push({
 
@@ -288,6 +289,7 @@ $("#submit-review").on("click", function (event) {
     recipe: recipeName,
     review: comment,
     rate: rating,
+    url: recipeURL,
 
   })
 
@@ -309,10 +311,11 @@ database.ref().on("child_added", function(childSnapshot) {
   var recipeTitle = (childSnapshot.val().recipe);
   var userReview = (childSnapshot.val().review);
   var userRating = (childSnapshot.val().rate);
+  var recURL = (childSnapshot.val().url);
   counter++;
 
-  $("#reviews").append("<div id='review-" + counter + "' class='four columns'><h5>" + userName + "</h5><h4>" + recipeTitle + "</h4><p>" + userReview + "</p><i class='star star-" + userRating + "'/></div>")
-  $("#reviewPage").append("<div id='review-section' class='twelve columns' <h5>" + userName + "</h5><h4>" + recipeTitle + "</h4><p>" + userReview + "</p><i class='star star-" + userRating + "'/><br></div>")
+  $("#reviews").append("<div id='review-" + counter + "' class='four columns'><h5>" + userName + "</h5><h4>" + recipeTitle + "</h4><a href= '" + recURL + "'>" + recURL + "</a><p>" + userReview + "</p><i class='star star-" + userRating + "'/></div>")
+  $("#reviewPage").append("<div id='review-section' class='twelve columns' <h5>" + userName + "</h5><h4>" + recipeTitle + "</h4><a href= '" + recURL + "'>" + recURL + "</a><p>" + userReview + "</p><i class='star star-" + userRating + "'/><br></div>")
   $(`#review-${counter - 3}`).remove();
 
 }, function (errorObj) {
