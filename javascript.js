fda9e020a47daab6954d4cbb445eaea9f3ee62ae
Ngoc Vo <ngocduyvo12@yahoo.com>
2019-09-25 11:27:50 -0600

    moved nutrition.js over to javascript.js

--- javascript.js
+++ javascript.js
@@ -81,12 +81,12 @@ function renderItems() {
 };
 $("#add-item").on("click", function (event) {
   event.preventDefault();
-  
+
   var itemName = $("#item").val().trim();
-  
-    if(!itemName){
-      return false;
-    }
+
+  if (!itemName) {
+    return false;
+  }
 
   items.push(itemName);
 
@@ -228,38 +228,129 @@ $(".list-of-recipes").on("click", ".recipeImg", displayVideo)
 $(".video").on("click", "#video-input", searchVideo)
 
 // saved Recipe collapsible
-$(".collapsible").on("click", function(){
+$(".collapsible").on("click", function () {
   if ($(".hidden").attr("class") === "hidden") {
     $(".hidden").attr("class", "show")
-  } else if($(".show").attr("class") === "show"){
+  } else if ($(".show").attr("class") === "show") {
     $(".show").attr("class", "hidden")
   }
 
 })
 
 //homepage image slideshow
-var homeImages =["images/breakfast.jpg", "images/burrito.jpg", "images/chocolate.jpg", "images/fruit.jpg", "images/macroons.jpg", "images/gather.jpg", "images/pasta.jpg", "images/pie.jpg", "images/protein.jpg", "images/vegebowl.jpg"]
+var homeImages = ["images/breakfast.jpg", "images/burrito.jpg", "images/chocolate.jpg", "images/fruit.jpg", "images/macroons.jpg", "images/gather.jpg", "images/pasta.jpg", "images/pie.jpg", "images/protein.jpg", "images/vegebowl.jpg"]
 
 var showImage;
 
 var homeImageCount = 0;
 
-function displayHomeImage(){
+function displayHomeImage() {
   $("#home-images-slideshow").html("<img class='w3-animate-fading' src=" + homeImages[homeImageCount] + " height='250px' width='100%'>").fadeIn()
   $("img").fadeIn()
 }
-function nextImage(){
+function nextImage() {
   homeImageCount++;
   setTimeout(displayHomeImage, 1000);
-  if (homeImageCount === homeImages.length){
+  if (homeImageCount === homeImages.length) {
     homeImageCount = 0;
   }
 }
-function startImageSlideshow(){
+function startImageSlideshow() {
   showImage = setInterval(nextImage, 6000)
 }
 displayHomeImage();
 startImageSlideshow();
+
+
+//nutrition.js
+$("#search-ingredient").on("click", function () {
+  event.preventDefault();
+
+  var ingredient = $("#nutritionInput").val().trim();
+  var ingredientEncode = ingredient.replace(" ", "%20")
+  var number = $("#qtyInput").val();
+  //create ratio to calculate serving
+  var ratio = number / 100;
+
+  if (!ingredient) {
+    $("#nutritionInput").attr("class", "input-false")
+  }
+  else {
+    $("#nutritionInput").attr("class", "input-true")
+  }
+
+  if (!number) {
+    $("#qtyInput").attr("class", "input-false")
+    return false;
+  }
+  else {
+    $("#qtyInput").attr("class", "input-true")
+  }
+
+  console.log(ingredientEncode)
+  //app key: ae848bd342699ecbf0e61c343bf3588c
+  //app id: 6ef4bc8b
+  //get nutrient per 100g
+  var queryURL = `https://api.edamam.com/api/food-database/parser?ingr=${ingredientEncode}&app_id=6ef4bc8b&app_key=ae848bd342699ecbf0e61c343bf3588c&nutrients`
+
+  console.log(queryURL)
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    //empty out existing result
+    $("#table-row").empty()
+
+    for (var i = 0; i < 5; i++) {
+      var label = response.hints[i].food.label;
+      var kCal = response.hints[i].food.nutrients.ENERC_KCAL; //energy in kcal
+      var kCal = kCal * ratio;
+      var fat = response.hints[i].food.nutrients.fat; //fat in g
+      if (fat) {
+        var fatTrimmed = fat * ratio;
+        var fatTrimmed = fatTrimmed.toFixed(2)
+      }
+
+      var dietFiber = response.hints[i].food.nutrients.FIBTG; //dietary fiber in g
+      if (dietFiber) {
+        var fiberTrimmed = dietFiber * ratio
+        var fiberTrimmed = fiberTrimmed.toFixed(2)
+      }
+
+      var protein = response.hints[i].food.nutrients.PROCNT; //protein
+      if (protein) {
+        var proteinTrimmed = protein * ratio
+        var proteinTrimmed = proteinTrimmed.toFixed(2)
+      }
+
+      var carb = response.hints[i].food.nutrients.CHOCDF; //carb in g
+      if (carb) {
+        var carbTrimmed = carb * ratio
+        var carbTrimmed = carbTrimmed.toFixed(2)
+      }
+
+
+      console.log(fatTrimmed)
+      console.log(fiberTrimmed)
+      console.log(proteinTrimmed)
+      console.log(carbTrimmed)
+
+
+
+      //create table:
+      $("#table-row").append(`<tr>
+        <th>${number}</th>
+        <th>grams</th>
+        <th>${label}</th>
+        <th>${kCal.toFixed(2)} kCal</th>
+        <th>Fat: ${fatTrimmed || "0"} g<br>Dietary Fiber: ${fiberTrimmed || "0"} g<br>Protein: ${proteinTrimmed || "0"} g<br>Carb: ${carbTrimmed || "0"} g<br></th>
+        </tr>`)
+
+    }
+  })
+})
 
 // Star Functionality
 $(document).ready(function () {
@@ -314,7 +405,7 @@ $(".rating-stars .star").on("click", function () {
 
 })
 
-database.ref().on("child_added", function(childSnapshot) {
+database.ref().on("child_added", function (childSnapshot) {
   var userName = (childSnapshot.val().name);
   var recipeTitle = (childSnapshot.val().recipe);
   var userReview = (childSnapshot.val().review);
@@ -355,26 +446,28 @@ $("#done-list").on("click", function (event) {
   oldItems = JSON.parse(localStorage.getItem("oldItems"));
   oldList = JSON.parse(localStorage.getItem("oldList"));
 
-   renderOldList();
+  renderOldList();
 
-   items = [];
-   list = [];
- 
-})  
+  items = [];
+  list = [];
+
+})
 
 
 
 function renderOldList() {
 
-  for(var i = 0; i < oldList.length; i++){
+  for (var i = 0; i < oldList.length; i++) {
     var listHtml = $("#old-shopping-lists")
     listHtml.prepend(`<li>${oldList[i]}</li>`)
   }
 
-  for(var i = 0; i < oldItems.length; i++){
+  for (var i = 0; i < oldItems.length; i++) {
     var listHtml = $("#old-shopping-lists")
     listHtml.prepend(`<li>${oldItems[i]}</li>`)
   }
 }
 
 renderOldList();
+
+
